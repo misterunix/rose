c8b232930aa9e6381c853abae0c410024a830480
Bill Jones <misterunix@gmail.com>
2023-09-06 20:38:51 -0500

    new version, manual now

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module rose
 
 go 1.17
 
-require github.com/misterunix/colorworks v0.0.1-gamma
+require (
+	github.com/misterunix/cgo-gd v0.0.1-rc1
+	github.com/misterunix/colorworks v0.0.1-gamma
+)
--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"fmt"
-	"image"
-	"image/color"
-	"image/draw"
-	"image/png"
-	"log"
 	"math"
-	"os"
+	"math/rand"
 
+	gd "github.com/misterunix/cgo-gd"
 	"github.com/misterunix/colorworks/hsl"
 )
 
@@ -18,7 +14,15 @@ const (
 	DEGTORAD = math.Pi / 180.0
 )
 
+type nd struct {
+	N int
+	D int
+}
+
+var ND []nd
+
 func main() {
+
 	// height : The height of the image.
 	height := 1000
 	// width : The width of the image.
@@ -27,81 +31,91 @@ func main() {
 	// amplitude : The amplitude of the wave.
 	amplitude := 0.45 * float64(width)
 
-	for i := 1; i < 20; i++ {
-		for j := 1; j < 20; j++ {
-			n := float64(j)                          // n : The numerator.
-			d := float64(i)                          // d : The denominator.
-			drawRose(width, height, n, d, amplitude) // Draw the rhodonea curve "rose".
+	/*
+		for i := 1; i < 20; i++ {
+			for j := 1; j < 20; j++ {
+				n := float64(j)                          // n : The numerator.
+				d := float64(i)                          // d : The denominator.
+				drawRose(width, height, n, d, amplitude) // Draw the rhodonea curve "rose".
+			}
 		}
-	}
-
+	*/
+	ND = append(ND, nd{4, 1})
+	ND = append(ND, nd{4, 2})
+	drawRose(width, height, amplitude)
 }
 
 // drawRose : Draw the rhodonea curve "rose"
-func drawRose(width, height int, n, d, amplitude float64) {
+func drawRose(width, height int, amplitude float64) {
 
-	rectboundingbox := image.Rectangle{}
-	rectboundingbox.Min.X = 0
-	rectboundingbox.Max.X = width
-	rectboundingbox.Min.Y = 0
-	rectboundingbox.Max.Y = height
+	l := len(ND)
+	n := make([]float64, l)
+	d := make([]float64, l)
+	for i := 0; i < l; i++ {
+		n[i] = float64(ND[i].N)
+		d[i] = float64(ND[i].D)
+	}
 
-	// Create a new image with the same bounds as the rectangle.
-	img := image.NewRGBA(rectboundingbox)
-	//c := color.White
+	img := gd.CreateTrueColor(width, height)
 
-	// bgColor : The background color of the image.
-	bgColor := color.RGBA{0, 0, 0, 255}
-	// Draw the background.
-	draw.Draw(img, img.Bounds(), &image.Uniform{bgColor}, image.Point{}, draw.Src)
+	bgColor := img.ColorAllocate(0, 0, 0)
+	img.Fill(0, 0, bgColor)
 
-	// Calculate the angular frequency.
-	k := n / d
+	for i := 0; i < l; i++ {
 
-	// loop over the curves by .001 increments
-	for s := 0.0; s < (360.0 * d); s = s + .001 {
+		// Calculate the angular frequency.
+		k := n[i] / d[i]
 
-		hue := math.Mod(s, 360.0) // hue : The hue of the color.
-		saturation := 1.0         // saturation : The saturation of the color.
-		level := .5               // level : The lightness of the color.
+		// Move the color around the color wheel.
+		hueOffset := rand.Float64() * 360.0
 
-		// Calculate the color. From the HSL color model.
-		r, g, b := hsl.HSLtoRGB(hue, saturation, level)
+		// loop over the curves by .001 increments
+		for s := 0.0; s < (360.0 * d[i]); s = s + .001 {
 
-		// cc : The color from the HSL color model in the RGB color model.
-		cc := color.RGBA{r, g, b, 0xff}
+			hue := math.Mod(s+hueOffset, 360.0) // hue : The hue of the color.
+			saturation := 1.0                   // saturation : The saturation of the color.
+			level := .5                         // level : The lightness of the color.
 
-		angle := s * DEGTORAD // angle : The angle of the curve.
+			// Calculate the color. From the HSL color model.
+			r, g, b := hsl.HSLtoRGB(hue, saturation, level)
 
-		// Cartesian coordinates
+			// cc : The color from the HSL color model in the RGB color model.
+			cc := img.ColorAllocate(int(r), int(g), int(b))
+			//cc := color.RGBA{r, g, b, 0xff}
 
-		x := amplitude * math.Cos(angle*k) * math.Cos(angle) // x : The x-coordinate of the point.
-		y := amplitude * math.Cos(angle*k) * math.Sin(angle) // y : The y-coordinate of the point.
+			angle := s * DEGTORAD // angle : The angle of the curve.
 
-		x1 := int(x) + (width / 2)  // x1 : The x-coordinate of the point centered on the image.
-		y1 := int(y) + (height / 2) // y1 : The y-coordinate of the point centered on the image.
+			// Cartesian coordinates
 
-		img.Set(x1, y1, cc) // Set the point (x, y) to color c in the image.
+			x := amplitude * math.Cos(angle*k) * math.Cos(angle) // x : The x-coordinate of the point.
+			y := amplitude * math.Cos(angle*k) * math.Sin(angle) // y : The y-coordinate of the point.
 
+			x1 := int(x) + (width / 2)  // x1 : The x-coordinate of the point centered on the image.
+			y1 := int(y) + (height / 2) // y1 : The y-coordinate of the point centered on the image.
+
+			img.FilledEllipse(x1, y1, 3, 3, cc)
+
+		}
 	}
 
 	//
 	// Save the image to a png file.
 	//
 
-	// filename : The name of the file.
-	filename := fmt.Sprintf("images/%d-%d.png", int(n), int(d))
-	// f : The file descriptor.
-	f, err := os.Create(filename)
-	if err != nil {
-		log.Fatalln(err) // Exit if the file cannot be created.
+	var filename string
+	filename = "images/"
+	for i := 0; i < l; i++ {
+		filename += fmt.Sprintf("%d-%d.", ND[i].N, ND[i].D)
+		/*
+			if i < l-1 {
+				filename += "-"
+			}
+		*/
 	}
-	defer f.Close() // Close the file when the function returns.
 
-	// Encode the image to the file.
-	err = png.Encode(f, img)
-	if err != nil {
-		log.Fatalln(err) // Exit if the image cannot be encoded.
-	}
+	// filename : The name of the file.
+	filename += "png" // := fmt.Sprintf("images/%d-%d.png", int(n), int(d))
+
+	img.Png(filename)
 
 }
